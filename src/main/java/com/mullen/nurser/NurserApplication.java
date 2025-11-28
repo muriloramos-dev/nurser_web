80fa741931cd233ed0e99078fd03845b5327ff94
muriloramos-dev <murilomassa72@gmail.com>
2025-11-27 23:17:28 -0300

    Initial commit

---
+++ src/main/java/com/mullen/nurser/NurserApplication.java
@@ -0,0 +1,13 @@
+package com.mullen.nurser;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NurserApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NurserApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/mullen/nurser/NurserApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mullen.nurser;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NurserApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
